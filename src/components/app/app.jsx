dc58a89a8d8dd292606fe924774819484ceee32b
theTylorNixon <bochkarevtrade@gmail.com>
2019-06-10 22:58:16 +0300

    router, hoc

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import './app.css';
+import { HomePage, CartPage } from '../pages';
+
+// import { withBookstoreService } from '../hoc';
+
 
 const App = () => {
     return (
-        <div>App</div>
-    )
+        <Switch>
+            <Route path="/"
+                component={HomePage}
+                exact
+            />
+
+            <Route path="/cart"
+                component={CartPage}
+            />
+        </Switch>
+    );
 };
 
 export default App;
+
+
+// const App = ({ bookstoreService }) => {
+//     console.log(bookstoreService.getBooks());
+//     return (
+//         <div>App</div>
+//     )
+// };
+
+// export default withBookstoreService()(App);
